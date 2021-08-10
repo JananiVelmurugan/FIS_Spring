643a8badc49f88e1d22cbbc93dc674008f5b0952
Janani Velmurugan <janani@hexaware.com>
2021-08-10 11:55:38 +0530

    added scope demos

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -3,9 +3,9 @@ package com.fis.springlearn;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,16 +16,17 @@ public class SpringLearnApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-		displayDate();
+//		SpringApplication.run(SpringLearnApplication.class, args);
+//		displayDate();
 		displayCountry();
 	}
 
 	static void displayCountry() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
 		Country country = context.getBean("country", Country.class);
-		LOGGER.debug("Country : {}", country.toString());
-
+		Country anotherCountry = context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country);
+		LOGGER.debug("Country : {}", anotherCountry);
 	}
 
 	static void displayDate() {
